a716567f5c2b58681cb51d9a26c6237159d474b2
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-23 16:40:25 +0200

    Dodani konstruktori i neke metode

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -2,5 +2,56 @@ package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
 
+    double pocetnaTacka, krajnjaTacka;
+    boolean pripadaPocetna, pripadaKrajnja;
 
+    Interval(double pocetnaTacka, double krajnjaTacka, boolean pripadaPocetna, boolean pripadaKrajnja) {
+        if (krajnjaTacka > pocetnaTacka) {
+
+        }
+
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pripadaPocetna = pripadaPocetna;
+        this.pripadaKrajnja = pripadaKrajnja;
+    }
+
+    Interval() {
+        pocetnaTacka = 0;
+        krajnjaTacka = 0;
+        pripadaPocetna = false;
+        pripadaKrajnja = false;
+    }
+
+    public boolean isNull() {
+        if (pocetnaTacka == 0 && krajnjaTacka == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isIn(double v) {
+        boolean pripadaPocetak = false;
+        boolean pripadaKraj = false;
+        if (v > pocetnaTacka)
+            pripadaPocetak = true;
+        if (v < krajnjaTacka)
+            pripadaKraj = true;
+        if (v == pocetnaTacka && pripadaPocetna)
+            pripadaPocetak = true;
+        if (v == krajnjaTacka && pripadaKrajnja)
+            pripadaKraj = true;
+        if (pripadaPocetak && pripadaKraj)
+            return true;
+        else
+            return false;
+    }
+
+    public Interval intersect(Interval interval){
+        return new Interval();
+    }
+
+    public static Interval intersect(Interval interval1, Interval interval2){
+        return new Interval();
+    }
 }
